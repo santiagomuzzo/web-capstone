a0e0ca45214efbef178bc6aae43debde9f70187e
joseh20p <jgmandujano@uc.cl>
2022-06-16 14:36:11 -0500

    navbar ok

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -6,10 +6,6 @@ import { Dropdown} from "react-bootstrap";
 
 import { loginRequest } from "../authConfig";
 import Button from '@mui/material/Button';
-import { borderRadius, color } from "@mui/system";
-import {FaBars} from "react-icons/fa";
-import styled from 'styled-components';
-import {MobileIcon} from './Header.elements'
 
 const buttonStyle = {
     padding: '22px',
@@ -31,9 +27,6 @@ export const Header = () => {
         <div className='navbar'>
             <div className="content">
             <Link to="/"><img src={logo} class="logo" alt="Main Logo" /></Link>  
-            <MobileIcon>
-                <FaBars/>
-            </MobileIcon>
 
             <AuthenticatedTemplate>
                     
@@ -56,7 +49,7 @@ export const Header = () => {
             <UnauthenticatedTemplate>
                 <ul>
                     <li>
-                        <Link to="/Proyects" style={{ textDecoration: 'none' }}><a className='boton-navbar' >Proyectos</a></Link>
+                        <Link to="/Proyects" style={{ textDecoration: 'none' }}><a className='boton-navbar' ><Button size = "small" style={buttonStyle}>Proyectos</Button></a></Link>
                     </li>
                     
                 </ul>
