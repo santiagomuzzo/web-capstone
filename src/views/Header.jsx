c13d480dc43515caedfaee9056f0e58c91265ea7
joseh20p <jgmandujano@uc.cl>
2022-06-16 14:31:44 -0500

    navbar

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -7,32 +7,42 @@ import { Dropdown} from "react-bootstrap";
 import { loginRequest } from "../authConfig";
 import Button from '@mui/material/Button';
 import { borderRadius, color } from "@mui/system";
+import {FaBars} from "react-icons/fa";
+import styled from 'styled-components';
+import {MobileIcon} from './Header.elements'
 
 const buttonStyle = {
     padding: '22px',
-    borderRadius: '0px'
+    borderRadius: '0px',
+    color: '#000000',
+    fontSize: '0.6rem'
 };
+
+
+
+
+    
+
+
 export const Header = () => {
     const { instance } = useMsal();
     
     return (
         <div className='navbar'>
             <div className="content">
-            <Link to="/"><img src={logo} class="logo" alt="Main Logo" /></Link>    
+            <Link to="/"><img src={logo} class="logo" alt="Main Logo" /></Link>  
+            <MobileIcon>
+                <FaBars/>
+            </MobileIcon>
+
             <AuthenticatedTemplate>
-                
+                    
                     <ul>
                         <li>
-                            <Link to="/Proyects" style={{ textDecoration: 'none' }}><Button size = "small" style={buttonStyle}><a className='boton-navbar' >Proyectos</a></Button></Link>
+                            <Link to="/Proyects" style={{ textDecoration: 'none' }}><a className='boton-navbar' ><Button size = "small" style={buttonStyle}>Proyectos</Button></a></Link>
                         </li>
                         <li>
-                            <Link to="/Dashboard" style={{ textDecoration: 'none' }}><Button size = "small" style={buttonStyle}><a className='boton-navbar' >Tablero</a></Button></Link>
-                        </li>
-                        <li>
-                            <Link to="/" style={{ textDecoration: 'none' }}><Button size = "small" style={buttonStyle}><a className='boton-navbar' >Analisis</a></Button></Link>
-                        </li>
-                        <li>
-                            <Link to="/" style={{ textDecoration: 'none' }}><Button size = "small" style={buttonStyle}><a className='boton-navbar' >Informes</a></Button></Link>
+                            <Link to="/Dashboard" style={{ textDecoration: 'none' }}><a className='boton-navbar' ><Button size = "small" style={buttonStyle}>Tablero</Button></a></Link>
                         </li>
                     </ul>
                 
